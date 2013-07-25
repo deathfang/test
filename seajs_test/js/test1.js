1b167e404648740ce399bf5d723fbf4867ebc699
deathfang <aamxy123@gmail.com>
2013-07-25 14:37:44 +0800

    no message

--- seajs_test/js/test1.js
+++ seajs_test/js/test1.js
@@ -2,9 +2,9 @@ define(function(require, exports, module) {
     var test2 = require.async('js/test2')
     function Test(){
         this.init = function(){
-            $('.test1 .test1').click(function(){
+            // $('.test1 .test1').click(function(){
                 test2.init()
-            })
+            // })
         }
     }
     module.exports = new Test
--- seajs_test/js/test2.js
+++ seajs_test/js/test2.js
@@ -1,9 +1,16 @@
 define(function(require, exports, module) {
 
-    var Test2 = function(){
-        this.init = function(){
-            setTimeout(alert('ok'));
-        }
-    }
-    module.exports = new Test2
-})
+    // var Test2 = function(){
+    //     this.init = function(){
+    //         setTimeout(alert('ok'));
+    //     }
+    // }
+    // module.exports = new Test2
+    exports.init = function(){
+        
+        $('.test1 .test1').click(function(){
+                setTimeout(alert('ok'));
+            })
+        }    
+})        
+        
